0e6c324e34f322582facacd91806cc81fed3bf15
Tres Serio <treserio@hotmail.com>
2022-11-11 10:41:02 -0600

    0x00 init

---
+++ 0x00-hello-dart/2-print_number.dart
@@ -0,0 +1,7 @@
+void printNumber(int aNumber) {
+  print('${aNumber} Battery street\n');
+}
+
+void main() {
+  printNumber(98);
+}
---
+++ 0x00-hello-dart/3-print_double.dart
@@ -0,0 +1,7 @@
+import 'dart:math';
+
+// print a rounded double to the second decimal place
+void main() {
+  double pi = 3.14159265359;
+    print('Double: ${pi.toStringAsFixed(2)}\n');
+}
