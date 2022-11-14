77356dab65145a7855d061edc7d49933fe7194dc
Tres Serio <treserio@hotmail.com>
2022-11-13 18:17:12 -0600

    4.1 fixed syntax

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -3,7 +3,7 @@ void outer(String name, String id) {
     String firstname = name.split(" ")[0];
     String lastname = name.substring(
         name.indexOf(" ") + 1,
-        name.indexOf(" ") +
+        name.indexOf(" ") + 2
     );
     return "Hello Agent $lastname.$firstname your id is $id";
   }
