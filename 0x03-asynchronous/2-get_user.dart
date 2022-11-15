5fd1ede095c18f6a2ca177f13957768e33b9fb47
Tres Serio <treserio@hotmail.com>
2022-11-15 13:24:36 -0600

    2 init

---
+++ 0x03-asynchronous/2-get_user.dart
@@ -0,0 +1,16 @@
+Future<String> fetchUserData() =>
+    // Imagine that this function is
+    // more complex and slow.
+    Future.delayed(
+        const Duration(seconds: 2),
+        () => throw 'Cannot locate user',
+    );
+
+Future<void> getUser() async {
+    fetchUserData()
+        .then((data) => print(data))
+        .catchError((err) => print('error caught: $err'));
+}
+main() async {
+    await getUser();
+}
