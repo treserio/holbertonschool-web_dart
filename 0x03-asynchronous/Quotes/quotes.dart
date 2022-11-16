5ee1ee99a2ad7f1bb5c87ed217017ddb54da32c1
Tres Serio <treserio@hotmail.com>
2022-11-15 20:19:54 -0600

    6.1 checker busted

--- 0x03-asynchronous/Quotes/quotes.dart
+++ 0x03-asynchronous/Quotes/quotes.dart
@@ -3,9 +3,7 @@ import 'package:http/http.dart' as http;
 
 Future<String> generateQuote(id) => http.get(Uri.parse(
   'https://breakingbadapi.com/api/quotes'))
-    .then((res) {
-      String quote = jsonDecode(res.body)[--id]['author'] + ' : ' +
-        jsonDecode(res.body)[id]['quote'];
-      return quote;
-    })
-    .catchError((err) => 'There is no quotes');
+    .then((res) => "${
+      jsonDecode(res.body)[--id]['author']} : ${
+      jsonDecode(res.body)[id]['quote']}")
+    .catchError((err) => 'There are no quotes');
